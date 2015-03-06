377fba91eefbbb3b0026db0cf5976a37e08c6996
Elliot Huffman <thelich2@gmail.com>
2015-03-06 15:38:10 -0500

    Update Auto Setup.py

--- Auto Setup.py
+++ Auto Setup.py
@@ -1,40 +1,52 @@
 __author__ = 'Elliot'
 from subprocess import call
+import platform
+def clear():
+    if platform.system() == "Windows":
+        call("cls")
+    elif platform.system() == "Darwin" or platform.system() == "Linux":
+        clear()
 def installsamba():
     call(["sudo", "apt-get", "update"])
     call(["sudo", "apt-get", "-y", "dist-upgrade"])
     call(["sudo", "apt-get", "install", "python-dnspython", "dnsutils", "attr", "krb5-user", "docbook-xsl", "acl", "samba"])
+    mainmenu()
 
 # This installs all of the necessary components for building samba then downloads samba from git.
 # After that it then compiles and installs samba and returns to the main menu.
 
 def installvsftpd():
+    clear()
     call(["sudo", "apt-get", "update"])
     call(["sudo", "apt-get", "-y", "dist-upgrade"])
     call(["sudo", "apt-get", "-y", "install", "vsftpd"])
+    mainmenu()
 # This installs VSFTPd and return to the main menu.
 
 
 def installwebmin():
+    clear()
     call(["wget", "http://www.webmin.com/download/deb/webmin-current.deb"])
     call(["sudo", "apt-get", "update"])
     call(["sudo", "apt-get", "-y", "dist-upgrade"])
     call(["sudo", "dpkg", "-i", "webmin-current.deb"])
     call(["sudo", "apt-get", "-y", "install", "-f"])
     call(["rm", "webmin-current.deb"])
+    mainmenu()
 
 # This installs the current version of WebMin and then returns to the main menu.
 
 def updatesystem():
     call(["sudo", "apt-get", "update"])
     call(["sudo", "apt-get", "-y", "dist-upgrade"])
+    clear()
     print("Update Complete!")
     print("It may be wise to restart your computer.")
     print("")
     print("Press Enter/Return to continue...")
     input()
  #   call(["read", "-n", "1"])
-    call("clear")
+    mainmenu()
 
 # This install the latest updates for the system and then returns to the main menu.
 
@@ -46,11 +58,13 @@ def configuresambaforactivedirectory():
     call(["sudo", "samba-tool", "domain", "provision", "--use-rfc2307", "--interactive"])
     call(["sudo", "mv", "/etc/krb5.conf", "/etc/krb5.conf.bak"])
     call(["sudo", "cp", "/var/lib/samba/private/krb5.conf", "/etc/krb5.conf"])
+    mainmenu()
 
 # This function runs all of the necessary actions to make samba a domain controller.
 
 
-def domaincontrolleryorn ():
+def domaincontrolleryorn():
+    clear()
     print("did you set this instalation as a primary domain controller?")
     print("")
     print("If you select yes then it will upgrade the forrest and domain to")
@@ -62,8 +76,9 @@ def domaincontrolleryorn ():
     domaincontrolleryesorno = str(input("Y or N:"))
     if domaincontrolleryesorno == "Y" or domaincontrolleryesorno == "y":
         upgradeforrestanddomain()
+        mainmenu()
     elif domaincontrolleryesorno == "N" or domaincontrolleryesorno == "n":
-        call("clear")
+        clear()
         print("Samba configuration complete!")
         print("Press Enter/Return to continue...")
         input()
@@ -75,13 +90,13 @@ def domaincontrolleryorn ():
         print("Press Enter/Return to continue...")
         input()
 #        call(["read", "-n", "1"])
-        call("clear")
         domaincontrolleryorn()
 
 # This asks the user if he or she would like to upgrade the domain and forrest level.
 # If yes then it roputs the user to the code below. If not then the user is taken to the main menu.
 
 def upgradeforrestanddomain():
+    clear()
     call(["sudo", "samba-tool", "domain level", "raise", "--domain-level=2008_R2"])
     call(["sudo", "samba-tool", "domain", "level", "raise", "--forest-level=2008_R2"])
     call(["sudo", "samba-tool", "domain", "passwordsettings", "set", "--complexity=off"])
@@ -90,7 +105,6 @@ def upgradeforrestanddomain():
     print("Press Enter/Return to return to the main menu...")
     input()
 #    call(["read", "-n", "1"])
-    call("clear")
     mainmenu()
 
 
@@ -99,7 +113,7 @@ def upgradeforrestanddomain():
 
 
 def quitprogram():
-    call("clear")
+    clear()
     print("Sorry to see you go... :(")
     exit()
 # This is a simple good by program closer.
@@ -107,7 +121,7 @@ def quitprogram():
 
 
 def mainmenu():
-    call("clear")
+    clear()
     print("Press 1 to update your system")
     print("Press 2 to install samba")
     print("Press 3 to install vsFTPd")
@@ -117,31 +131,25 @@ def mainmenu():
     print("")
     mainmenuinput = str(input("Input Selection:"))
     if mainmenuinput == "1":
-        call("clear")
         updatesystem()
     elif mainmenuinput == "2":
-        call("clear")
         installsamba()
     elif mainmenuinput == "3":
-        call("clear")
         installvsftpd()
     elif mainmenuinput == "4":
-        call("clear")
         installwebmin()
     elif mainmenuinput == "5":
-        call("clear")
         configuresambaforactivedirectory()
     elif mainmenuinput == "x" or mainmenuinput == "X":
-        call("clear")
         quitprogram()
     else:
+        clear()
         print("You have entered an invallid selection!")
         print("Please try again!")
         print("")
         print("Press Return/Enter to continue...")
         input()
 #        call(["read", "-n", "1"])
-        call("clear")
         mainmenu()
 
 # This builds the main menu and routs the user to the function selected.
